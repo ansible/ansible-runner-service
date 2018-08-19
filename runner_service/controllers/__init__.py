5cff3d5ea0dcf784e50f85960e12a670696ddbb1
Paul Cuzner <pcuzner@redhat.com>
2018-08-20 09:41:06 +1200

    Add host management API endpoints
    Add host management entry points to the API to
    allow the host to be added/updated and removed
    from Ansible control
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- runner_service/controllers/__init__.py
+++ runner_service/controllers/__init__.py
@@ -1,5 +1,5 @@
 
 from .playbooks import ListPlaybooks, PlaybookState, StartPlaybook
 from .api import API
-from .hosts import Hosts
+from .hosts import Hosts, HostMgmt, HostUpdate
 from .jobs import ListEvents, GetEvent
--- runner_service/controllers/hosts.py
+++ runner_service/controllers/hosts.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from .utils import requires_auth
+from .utils import requires_auth, log_request
 
 import logging
 logger = logging.getLogger(__name__)
@@ -9,10 +9,55 @@ class Hosts(Resource):
     """Return a list of hosts from the inventory - PLACEHOLDER"""
 
     @requires_auth
+    @log_request(logger)
     def get(self):
         """
         GET
         Return a list of hosts known to the ansible inventory - NOT IMPLEMENTED
         """
 
-        return 501
+        return {"message": "NOT Implemented"}, 501
+
+
+class HostMgmt(Resource):
+    """Manage ansible control of a given host - PLACEHOLDER"""
+
+    @requires_auth
+    @log_request(logger)
+    def get(self):
+        """
+        GET {host_name}
+        Return the groups that the given host is a member of
+        """
+        return {"message": "NOT Implemented"}, 501
+
+    @requires_auth
+    @log_request(logger)
+    def post(self):
+        """
+        POST {host_name}
+        Add a new host to the ansible configuration
+        """
+        return {"message": "NOT Implemented"}, 501
+
+    @requires_auth
+    @log_request(logger)
+    def delete(self):
+        """
+        POST {host_name}
+        Remove a host from ansible control
+        """
+        return {"message": "NOT Implemented"}, 501
+
+
+class HostUpdate(Resource):
+    """Manage group membership of an existing host - PLACEHOLDER"""
+
+    @requires_auth
+    @log_request(logger)
+    def put(self):
+        """
+        PUT {host_name, group_name}
+        Update the groups a given host belongs to
+        """
+        return {"message": "NOT Implemented"}, 501
