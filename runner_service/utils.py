52aea885a7a28a6a710f67e4163b76688a388949
Paul Cuzner <pcuzner@redhat.com>
2018-08-20 15:06:58 +1200

    Add SSL support
    To prevent on the wire snooping, the app now looks
    for crt and key files in it's config directory.
    
    If these files are missing they're created for
    you, and used to define an SSL context for the
    flask app.
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- runner_service/utils.py
+++ runner_service/utils.py
@@ -1,3 +1,10 @@
+import os
+
+from socket import gethostname
+from OpenSSL import crypto
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class RunnerServiceError(Exception):
@@ -8,3 +15,53 @@ def fread(file_path):
     """ return the contents of the given file """
     with open(file_path, 'r') as file_fd:
         return file_fd.read().strip()
+
+
+def create_self_signed_cert(cert_dir, cert_pfx):
+    """
+    Looks in cert_dir for the key files (using the cert_pfx name), and either
+    returns if they exist, or create them if they're missing.
+    """
+
+    cert_filename = os.path.join(cert_dir,
+                                 "{}.crt".format(cert_pfx))
+    key_filename = os.path.join(cert_dir,
+                                "{}.key".format(cert_pfx))
+
+    logger.debug("Checking for the SSL keys in {}".format(cert_dir))
+    if os.path.exists(cert_filename) \
+            or os.path.exists(key_filename):
+        logger.info("Using existing SSL files in {}".format(cert_dir))
+        return (cert_filename, key_filename)
+    else:
+        logger.info("Existing SSL files not found in {}".format(cert_dir))
+        logger.info("Self-signed cert will be created")
+
+        # create a key pair
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 1024)
+
+        # create a self-signed cert
+        cert = crypto.X509()
+        cert.get_subject().C = "US"
+        cert.get_subject().ST = "North Carolina"
+        cert.get_subject().L = "Raliegh"
+        cert.get_subject().O = "Red Hat"
+        cert.get_subject().OU = "Ansible"
+        cert.get_subject().CN = gethostname()
+        cert.set_serial_number(1000)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(3 * 365 * 24 * 60 * 60)    # 3 years
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, 'sha1')
+
+        logger.debug("Writing crt file to {}".format(cert_filename))
+        with open(os.path.join(cert_dir, cert_filename), "wt") as cert_fd:
+            cert_fd.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
+        logger.debug("Writing key file to {}".format(key_filename))
+        with open(os.path.join(cert_dir, key_filename), "wt") as key_fd:
+            key_fd.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+        return (cert_filename, key_filename)
