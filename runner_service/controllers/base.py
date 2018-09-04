1f3e3cdb8babdbcaeb9eb1aeb34d28ed7d1a7e57
Paul Cuzner <pcuzner@redhat.com>
2018-09-04 16:53:25 +1200

    Base class added (extending Resource)
    Just implements a class based on Resource that
    provides a status to http code lookup.
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

---
+++ runner_service/controllers/base.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+
+class BaseResource(Resource):
+    state_to_http = {
+        "OK": 200,
+        "STARTED": 202,
+        "INVALID": 400,
+        "NOAUTH": 401,
+        "NOTFOUND": 404,
+        "LOCKED": 409,
+        "FAILED": 500,
+        "TIMEOUT": 504
+    }
