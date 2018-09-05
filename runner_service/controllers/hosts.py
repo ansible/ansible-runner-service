253aeb9ff15c0102112f87de9ad7f182980e2a6b
Paul Cuzner <pcuzner@redhat.com>
2018-09-05 17:03:29 +1200

    Add usage examples for the hosts api
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- runner_service/controllers/hosts.py
+++ runner_service/controllers/hosts.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 class Hosts(BaseResource):
-    """Return a list of hosts from the inventory - PLACEHOLDER"""
+    """Return a list of hosts from the inventory"""
 
     @requires_auth
     @log_request(logger)
@@ -20,6 +20,29 @@ class Hosts(BaseResource):
         """
         GET
         Return all hosts from the ansible inventory
+
+        Example.
+
+        ```
+        $ curl -k -i https://localhost:5001/api/v1/hosts -X get
+        HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 150
+        Server: Werkzeug/0.14.1 Python/3.6.6
+        Date: Wed, 05 Sep 2018 04:58:31 GMT
+
+        {
+            "status": "OK",
+            "msg": "",
+            "data": {
+                "hosts": [
+                    "con-1",
+                    "con-2",
+                    "con-3"
+                ]
+            }
+        }
+        ```
         """
 
         response = get_hosts()
@@ -28,7 +51,7 @@ class Hosts(BaseResource):
 
 
 class HostDetails(BaseResource):
-    """Manage ansible control of a given host - PLACEHOLDER"""
+    """Show group membership for a given host"""
 
     @requires_auth
     @log_request(logger)
@@ -36,6 +59,27 @@ class HostDetails(BaseResource):
         """
         GET {host_name}
         Return the groups that the given host is a member of
+
+        Example.
+
+        ```
+        $ curl -k -i https://localhost:5001/api/v1/hosts/con-1 -X get
+        HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 108
+        Server: Werkzeug/0.14.1 Python/3.6.6
+        Date: Wed, 05 Sep 2018 04:59:05 GMT
+
+        {
+            "status": "OK",
+            "msg": "",
+            "data": {
+                "groups": [
+                    "osds"
+                ]
+            }
+        }
+        ```
         """
 
         response = get_host_membership(host_name)
@@ -44,14 +88,31 @@ class HostDetails(BaseResource):
 
 
 class HostMgmt(BaseResource):
-    """Manage ansible control of a given host - PLACEHOLDER"""
+    """Manage ansible control of a given host"""
 
     @requires_auth
     @log_request(logger)
     def post(self, host_name, group_name):
         """
         POST hosts/{host_name}/groups/{group_name}
-        Add a new host to a group in the ansible inventory
+        Add a new host to an existing group in the ansible inventory
+
+        Example.
+
+        ```
+        $ curl -k -i https://localhost:5001/api/v1/hosts/con-1/groups/dummy -X post
+        HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 54
+        Server: Werkzeug/0.14.1 Python/3.6.6
+        Date: Wed, 05 Sep 2018 05:00:33 GMT
+
+        {
+            "status": "OK",
+            "msg": "",
+            "data": {}
+        }
+        ```
         """
 
         response = add_host(host_name, group_name)
@@ -62,7 +123,24 @@ class HostMgmt(BaseResource):
     def delete(self, host_name, group_name):
         """
         DELETE hosts/{host_name}/group/{group_name}
-        Remove a host from ansible group
+        Remove a host from an ansible group
+
+        Example.
+
+        ```
+        $ curl -k -i https://localhost:5001/api/v1/hosts/con-1/groups/dummy -X delete
+        HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 54
+        Server: Werkzeug/0.14.1 Python/3.6.6
+        Date: Wed, 05 Sep 2018 05:02:03 GMT
+
+        {
+            "status": "OK",
+            "msg": "",
+            "data": {}
+        }
+        ```
         """
 
         response = remove_host(host_name, group_name)
