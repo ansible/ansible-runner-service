0b3b4fb772273e47b6204c980d88b774918eb337
Paul Cuzner <pcuzner@redhat.com>
2018-09-24 16:24:54 +1200

    Update inventory mgmt to use yaml.safe_dump
    yaml dump differs between python2 and python3 -
    with safe_dump the output returned is always a
    str object which the file write method needs.
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- runner_service/inventory.py
+++ runner_service/inventory.py
@@ -109,16 +109,22 @@ class AnsibleInventory(object):
     def load(self):
 
         if not os.path.exists(self.filename):
+
             try:
-                # Using Python 3 exclusive creation
-                with open(self.filename, 'x') as inv:
-                    inv.write(yaml.dump(AnsibleInventory.inventory_seed,
-                              default_flow_style=False))
-            except FileExistsError:
-                logger.info("Inventory file '{}' already created".format(self.filename))
-            except IOError:
-                raise InventoryWriteError("Unable to create the seed inventory"
-                                          " file at {}".format(self.filename))
+                # could use Python 3 exclusive creation open(file, 'x'), but..
+                self.fd = open(self.filename, 'w')
+                fcntl.flock(self.fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except (BlockingIOError, OSError) as _e:
+                logger.warning("Race condition hit creating Inventory file")
+            else:
+                try:
+                    self.fd.write(yaml.safe_dump(AnsibleInventory.inventory_seed,   # noqa
+                                                 default_flow_style=False))
+                    fcntl.flock(self.fd, fcntl.LOCK_UN)
+                except IOError as _e:
+                    raise InventoryWriteError("Seeding inventory failed: {}".format(_e))    # noqa
+            finally:
+                self.fd.close()
 
         try:
             if self.exclusive_lock:
@@ -134,8 +140,8 @@ class AnsibleInventory(object):
             else:
                 raw = fread(self.filename)
         except Exception as ex:
-            raise InventoryreadError("Unable to read the inventory"
-                                     " file at {}, error: {}".format(self.filename, ex))
+            raise InventoryreadError("Unable to read the inventory file at "
+                                     "{}, error: {}".format(self.filename, ex))
 
         if not raw:
             # If the inventory is empty for some extrange reason
@@ -149,7 +155,7 @@ class AnsibleInventory(object):
                                             " yaml file at {}, error: {}".format(self.filename, ex))
 
     def _dump(self):
-        return yaml.dump(self.inventory, default_flow_style=False)
+        return yaml.safe_dump(self.inventory, default_flow_style=False)
 
     def save(self):
         # Changes in inventory only allowed with exclusive lock
