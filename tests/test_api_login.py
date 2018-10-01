94f08162f06ba630b30a885d732d17d15fdc5e87
Paul Cuzner <pcuzner@redhat.com>
2018-10-01 15:06:36 +1300

    Restructured to use flask test client
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -1,12 +1,13 @@
 
 import sys
+import json
 import logging
-import requests
+
 import unittest
 
 sys.path.extend(["../", "./"])
 from common import APITestCase  # noqa
-
+from ansible_runner_service import main     # noqa E402
 
 # turn of normal logging that the ansible_runner_service will generate
 nh = logging.NullHandler()
@@ -18,39 +19,38 @@ class TestLogin(APITestCase):
 
     def test_good_login(self):
         """- test happy state login"""
-        url = "https://localhost:{}/api/v1/login".format(self.config.port)
-        response = requests.get(url,
-                                auth=('admin', 'admin'),
-                                verify=False)
+
+        response = self.app.get('/api/v1/login',
+                                headers=self.auth_header('admin', 'admin'))
         self.assertEqual(response.status_code,
                          200)
-        self.assertIn('token', response.json()['data'])
+        self.assertIn('token', json.loads(response.data)['data'])
 
     def test_bad_password(self):
         """- test bad password"""
-        url = "https://localhost:{}/api/v1/login".format(self.config.port)
-        response = requests.get(url,
-                                auth=('admin', 'wah'),
-                                verify=False)
+
+        response = self.app.get('/api/v1/login',
+                                headers=self.auth_header('admin', 'wah'))
+
         self.assertEqual(response.status_code,
                          401)
-        self.assertIn('password incorrect', response.json()['msg'])
+        self.assertIn('password incorrect', json.loads(response.data)['msg'])
 
     def test_bad_user(self):
         """- test bad username"""
-        url = "https://localhost:{}/api/v1/login".format(self.config.port)
-        response = requests.get(url,
-                                auth=('wee', 'wah'),
-                                verify=False)
+
+        response = self.app.get('/api/v1/login',
+                                headers=self.auth_header('wee', 'wah'))
+
         self.assertEqual(response.status_code,
                          401)
-        self.assertIn('unknown user', response.json()['msg'])
+        self.assertIn('unknown user', json.loads(response.data)['msg'])
 
     def test_access_without_token(self):
         """- test access to a protected endpoint, without token"""
-        url = "https://localhost:{}/api/v1/groups".format(self.config.port)
-        response = requests.get(url,
-                                verify=False)
+
+        response = self.app.get('/api/v1/groups')
+
         self.assertEqual(response.status_code,
                          401)
 
