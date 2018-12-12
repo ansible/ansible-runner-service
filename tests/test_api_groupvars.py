500cdbdec472233333ab6eba1273a319e7edaf08
Paul Cuzner <pcuzner@redhat.com>
2018-12-12 14:40:23 +1300

    Added testcases for hostvars and groupvars support
    Tests cases(x22) exercises the hostvars and
    groupvars API endpoints.
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

---
+++ tests/test_api_groupvars.py
@@ -0,0 +1,156 @@
+
+import sys
+import os
+import json
+import yaml
+import unittest
+
+sys.path.extend(["../", "./"])
+from common import APITestCase  # noqa
+from runner_service.utils import fread      # noqa
+
+
+class TestGroupVars(APITestCase):
+
+    def test_list_invalid_groupvars(self):
+        """- list groupvars for a non-existent group"""
+        response = self.app.get('api/v1/groupvars/wobbles',
+                                headers=self.token_header())
+
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_invalid_groupvars(self):
+        """ - create group vars for non-existent group"""
+
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/groupsvars/gv1',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_invalid_payload(self):
+        """ - create group vars with non json data"""
+        response = self.app.post('api/v1/groups/gv2',
+                                 headers=self.token_header())
+
+        response = self.app.post('api/v1/groupvars/gv2',
+                                 data="invalid data",
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         400)
+
+    def test_add_invalid_content(self):
+        """ - create group vars but without json contenttype"""
+        response = self.app.post('api/v1/groups/gv3',
+                                 headers=self.token_header())
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/groupvars/gv3',
+                                 data=json.dumps(payload),
+                                 content_type="text/html",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         415)
+
+    def test_delete_invalid_groupvars(self):
+        """ - attempt to delete a non-existent groupvars entry"""
+        response = self.app.delete("api/v1/groupvars/gv1",
+                                   headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_groupvars_file(self):
+        """ - add a group and groupvars (file)"""
+        payload = {"var1": "value1"}
+
+        response = self.app.post('api/v1/groups/gvadd',
+                                 headers=self.token_header())
+        response = self.app.post('api/v1/groupvars/gvadd',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+        cwd = os.getcwd()
+        self.assertTrue(os.path.exists(
+                          os.path.join(cwd,
+                                       'samples/project/group_vars/gvadd.yml'))) # noqa E501
+
+    def test_add_groupvars_inventory(self):
+        """ - add a group and groupvars (inventory - hosts)"""
+        payload = {"var1": "value1"}
+
+        response = self.app.post('api/v1/groups/gvaddinv',
+                                 headers=self.token_header())
+        response = self.app.post('api/v1/groupvars/gvaddinv?type=inventory',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+        cwd = os.getcwd()
+        inv = yaml.safe_load(fread(os.path.join(cwd, 'samples/inventory/hosts'))) # noqa E501
+        self.assertIn('var1', inv['all']['children']['gvaddinv']['vars'])
+
+    def test_fetch_groupvars(self):
+        """- fetch group vars"""
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/groups/fetchvars',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/groupvars/fetchvars',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.get('api/v1/groupvars/fetchvars',
+                                headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        payload = json.loads(response.data)
+        self.assertIn('var1', payload['data']['vars'].keys())
+
+    def test_delete_groupvars_file(self):
+        """- delete groupvars (from group_vars dir)"""
+        payload = {"var1": "value1"}
+        response = self.app.post("api/v1/groups/gvdel",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/groupvars/gvdel',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.delete('api/v1/groupvars/gvdel',
+                                   headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        cwd = os.getcwd()
+        gvars_files = os.listdir(os.path.join(cwd, 'samples/project/group_vars')) # noqa E501
+        self.assertNotIn('gvdel', gvars_files)
+
+    def test_delete_groupvars_inv(self):
+        """- delete groupvars (from group_vars dir)"""
+        payload = {"var1": "value1"}
+        response = self.app.post("api/v1/groups/gvinventory",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/groupvars/gvinventory?type=inventory',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.delete('api/v1/groupvars/gvinventory',
+                                   headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        cwd = os.getcwd()
+        inv = yaml.safe_load(fread(os.path.join(cwd, 'samples/inventory/hosts'))) # noqa E501
+        self.assertNotIn('vars', inv['all']['children']['gvinventory'])
+
+
+if __name__ == "__main__":
+
+    unittest.main(verbosity=2)
---
+++ tests/test_api_hostvars.py
@@ -0,0 +1,165 @@
+
+import sys
+import os
+import json
+import yaml
+import unittest
+
+sys.path.extend(["../", "./"])
+from common import APITestCase  # noqa
+from runner_service.utils import fread      # noqa
+
+
+class TestHostVars(APITestCase):
+
+    def test_get_invalid_group(self):
+        """- fetch hostsvars - nonexistent group"""
+        response = self.app.get('api/v1/hostvars/bogus/groups/group1',
+                                headers=self.token_header())
+
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_get_invalid_host(self):
+        """- fetch hostsvars - group valid, host invalid"""
+        response = self.app.post('api/v1/groups/group2',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+
+        response = self.app.get('api/v1/hostvars/notallthere/groups/group2',
+                                headers=self.token_header())
+
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_hostvars_file(self):
+        """- create hostvars, group doesn't need to exist"""
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/host1/groups/hosts',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+
+    def test_add_hostvars_inventory_nogroup(self):
+        """- create hostvars, but group doesn't exist"""
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/host1/groups/missing?type=inventory', # noqa E501
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_hostvars_inventory_nohost(self):
+        """- create hostvars, inventory action - group OK, host NOTOK"""
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/groups/hosts',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/hostvars/host1/groups/hosts?type=inventory', # noqa E501
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         404)
+
+    def test_add_hostvars_invalid_data(self):
+        """- add hostvars, but data payload is invalid"""
+        response = self.app.post('api/v1/groups/tahid',
+                                 headers=self.token_header())
+
+        response = self.app.post('api/v1/hostvars/myhost/groups/tahid',
+                                 data="invalid data",
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         400)
+
+    def test_add_hostvars_request_invalid(self):
+        """- add hostvars, but content-type not set"""
+        response = self.app.post('api/v1/groups/tahri',
+                                 headers=self.token_header())
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/myhost/groups/tahri',
+                                 data=json.dumps(payload),
+                                 content_type="text/html",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         415)
+
+    def test_add_hostvars_inventory(self):
+        """- create hostvars (file), group and host valid"""
+        response = self.app.post('api/v1/groups/tahi',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/hosts/localhost/groups/tahi',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/localhost/groups/tahi?type=inventory', # noqa E501
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+        cwd = os.getcwd()
+        inv = yaml.safe_load(fread(os.path.join(cwd, 'samples/inventory/hosts'))) # noqa E501
+        self.assertIn('var1', inv['all']['children']['tahi']['hosts']['localhost']) # noqa
+
+    def test_fetch_hostvars_invalid(self):
+        """- fetch hostsvars - no such host"""
+        response = self.app.get('api/v1/hostvars/hosttfhi/groups/tfhi',
+                                headers=self.token_header())
+        self.assertEqual(response.status_code, 404)
+
+    def test_fetch_hostvars(self):
+        """- fetch hostvars"""
+        response = self.app.post('api/v1/groups/tfh',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/tfhhost/groups/tfh',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code,
+                         200)
+        response = self.app.get('api/v1/hostvars/tfhhost/groups/tfh',
+                                headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_hostsvars_invalid(self):
+        """- delete non-existent hostvars"""
+        response = self.app.delete('api/v1/hostvars/deleteme/group/unknown',
+                                   headers=self.token_header())
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_hostvars(self):
+        """- delete valid hostvars (inventory or file removed)"""
+        response = self.app.post('api/v1/groups/tdh',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+        response = self.app.post('api/v1/hosts/localhost/groups/tdh',
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        payload = {"var1": "value1"}
+        response = self.app.post('api/v1/hostvars/localhost/groups/tdh',
+                                 data=json.dumps(payload),
+                                 content_type="application/json",
+                                 headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+        response = self.app.delete('api/v1/hostvars/localhost/groups/tdh',
+                                   headers=self.token_header())
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+
+    unittest.main(verbosity=2)
